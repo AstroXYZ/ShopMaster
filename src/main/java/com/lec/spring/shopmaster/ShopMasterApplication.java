9765443c0424650809fe8a86d11b52a1c8e8b554
AstroXYZ <beomkyu0730@gmail.com>
2025-04-29 10:13:54 +0900

    Initial Commit

---
+++ src/main/java/com/lec/spring/shopmaster/ShopMasterApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring.shopmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lec/spring/shopmaster/ShopMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring.shopmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
